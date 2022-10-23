e1816b0f0b4f6ed4dccde2d39e62623676f696e2
Владимир Мелещук <vladimirmeleshuk@MacBook-Air-Vladimir.local>
2022-10-24 01:09:22 +0300

    Setup base project
    Create App and Sources folder. Move Assets, LaunchScreen, AppDelegate, SceneDelegate, Info to App folder. Move ViewController to Source folder. Configure SceneDelegate to display ViewController.

---
+++ ShoppingCart/Sources/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingCart
+//
+//  Created by Владимир Мелещук on 24.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
