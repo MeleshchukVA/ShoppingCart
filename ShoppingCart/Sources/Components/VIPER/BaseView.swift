45dceb3ec78d7b37924054799322a07facc4ce47
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-10-31 19:07:52 +0300

    Add BaseView to VIPER folder inside Components folder

---
+++ ShoppingCart/Sources/Components/VIPER/BaseView.swift
@@ -0,0 +1,17 @@
+//
+//  BaseView.swift
+//  ShoppingCart
+//
+//  Created by Владимир Мелещук on 31.10.2022.
+//
+
+import UIKit
+
+class BaseView: UIView {
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activity = UIActivityIndicatorView(style: .large)
+        activity.color = Colors.lightGray
+        return activity
+    }()
+}
