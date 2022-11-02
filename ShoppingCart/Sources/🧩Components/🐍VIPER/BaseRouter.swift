4482377f6b56a288f41ef35aac214845d9b5d266
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-02 18:27:25 +0300

    Create and configured BaseRouter in VIPER folder

---
+++ ShoppingCart/Sources/🧩Components/🐍VIPER/BaseRouter.swift
@@ -0,0 +1,22 @@
+//
+//  BaseRouter.swift
+//  ShoppingCart
+//
+//  Created by Владимир Мелещук on 02.11.2022.
+//
+
+import UIKit
+
+class BaseRouter {
+    var moduleDependencies: ModuleDependencies?
+    
+    var navigationControllerProvider: (() -> UINavigationController?)?
+    var navigationController: UINavigationController? {
+        self.navigationControllerProvider?()
+    }
+    
+    var viewControllerProvider: (() -> UIViewController?)?
+    var viewController: UIViewController? {
+        self.viewControllerProvider?()
+    }
+}
