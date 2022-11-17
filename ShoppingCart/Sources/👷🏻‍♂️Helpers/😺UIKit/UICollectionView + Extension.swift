797936675a69ce2079bf6f5b1a280daed5205914
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-17 21:03:02 +0300

    Add functions setEmptyMessage and restore in UICollectionView + Extension

--- ShoppingCart/Sources/👷🏻‍♂️Helpers/😺UIKit/UICollectionView + Extension.swift
+++ ShoppingCart/Sources/👷🏻‍♂️Helpers/😺UIKit/UICollectionView + Extension.swift
@@ -46,4 +46,24 @@ extension UICollectionView {
         
         return cell
     }
+    
+    func setEmptyMessage(message: String) {
+        let messageLabel = UILabel(frame: CGRect(
+            x: 0,
+            y: 0,
+            width: UIScreen.main.bounds.size.width,
+            height: UIScreen.main.bounds.size.height))
+        messageLabel.text = message
+        messageLabel.textColor = .gray
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        messageLabel.font = Font.sber(ofSize: Font.Size.twentyEight, weight: .regular)
+        messageLabel.sizeToFit()
+        
+        self.backgroundView = messageLabel
+    }
+    
+    func restore() {
+        self.backgroundView = nil
+    }
 }
