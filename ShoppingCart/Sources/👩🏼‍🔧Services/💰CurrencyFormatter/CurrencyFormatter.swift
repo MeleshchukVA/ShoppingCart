1a93ae4b97c7e61f584bea989352aa912845db1e
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-17 20:47:24 +0300

    Add CurrencyFormatter in Services

---
+++ ShoppingCart/Sources/👩🏼‍🔧Services/💰CurrencyFormatter/CurrencyFormatter.swift
@@ -0,0 +1,47 @@
+//
+//  CurrencyFormatter.swift
+//  ShoppingCart
+//
+//  Created by Владимир Мелещук on 17.11.2022.
+//
+
+import Foundation
+
+enum CurrencyFormatterLocale: String {
+    case russia = "ru_RU"
+    case usa = "us_US"
+}
+
+final class CurrencyFormatter: NumberFormatter {
+    
+    static let shared = CurrencyFormatter()
+    
+    private override init() {
+        super.init()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configurate(
+        numberStyle: NumberFormatter.Style = .currency,
+        localeType: CurrencyFormatterLocale = .usa
+    ) {
+        self.numberStyle = numberStyle
+        self.maximumFractionDigits = 2
+        self.minimumFractionDigits = 0
+        self.locale = Locale(identifier: localeType.rawValue)
+    }
+    
+    func string(from: Double) -> String? {
+        string(from: NSNumber(value: from))
+    }
+}
+
+extension CurrencyFormatter {
+    
+    override func copy(with zone: NSZone? = nil) -> Any {
+        return self
+    }
+}
