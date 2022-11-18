0ca7c991c63f15026cb529b7181e70d0954cafe9
Владимир Мелещук <vladimirmeleshuk@MacBook-Air-Vladimir.local>
2022-11-18 17:03:15 +0300

    Add ProductViewModel in Product module

---
+++ ShoppingCart/Sources/📚Modules/💻Product/ProductViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  ProductViewModel.swift
+//  ShoppingCart
+//
+//  Created by Владимир Мелещук on 18.11.2022.
+//
+
+import Foundation
+
+struct ProductViewModel: Identifiable {
+    var id: Int
+    var title: String
+    var description: String
+    var price: UInt
+    var discountPercentage: Double
+    var rating: Double
+    var stock: Int
+    var brand: String
+    var category: String
+    var thumbnail: String
+    var images: [String]
+    var didTap: (Int) -> Void
+}
