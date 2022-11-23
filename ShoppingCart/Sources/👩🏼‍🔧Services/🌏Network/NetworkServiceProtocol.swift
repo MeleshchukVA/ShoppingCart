5f7a8af30320b407b80cadbaffa709ab68a140a7
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-23 14:45:25 +0300

    Add another functions for fetching information from API

--- ShoppingCart/Sources/👩🏼‍🔧Services/🌏Network/NetworkServiceProtocol.swift
+++ ShoppingCart/Sources/👩🏼‍🔧Services/🌏Network/NetworkServiceProtocol.swift
@@ -10,4 +10,7 @@ import Foundation
 protocol NetworkServiceProtocol {
     func fetchCategories(completion: @escaping (Result<Categories, Error>) -> Void)
     func fetchProducts(with name: String, completion: @escaping (Result<Products, Error>) -> Void)
+    func fetchSearchedProducts(by query: String, skip: Int, completion: @escaping (Result<Products, Error>) -> Void)
+    func addCart(_ body: [String: Any], completion: @escaping (Result<AddCartResponse, Error>) -> Void)
+    func getUser(completion: @escaping (Result<User, Error>) -> Void)
 }
--- ShoppingCart/Sources/👩🏼‍🔧Services/🌏Network/URLFactory.swift
+++ ShoppingCart/Sources/👩🏼‍🔧Services/🌏Network/URLFactory.swift
@@ -8,26 +8,56 @@
 import Foundation
 
 enum URLFactory {
-    private static let baseURLComponents: URLComponents? = {
-        var urlComponents = URLComponents(string: API.main)
-        urlComponents?.queryItems = []
+    private static var baseURL: URL {
+        return baseURLComponents.url!
+    }
+    
+    private static let baseURLComponents: URLComponents = {
+        let url = URL(string: API.main)!
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        urlComponents.queryItems = []
         return urlComponents
     }()
     
-    private static var baseURL: URL? {
-        return baseURLComponents?.url
-    }
-    
-    static func getCategories() -> String? {
-        return baseURL?
+    static func getCategories() -> String {
+        let urlComponents = baseURLComponents
+        return urlComponents.url!
             .appendingPathComponent(API.TypeOf.categories)
             .absoluteString
     }
     
-    static func getProducts(name: String) -> String? {
-        return baseURL?
+    static func getProducts(name: String) -> String {
+        let urlComponents = baseURLComponents
+        return urlComponents.url!
             .appendingPathComponent(API.TypeOf.products)
             .appendingPathComponent(name)
+            .absoluteString
+    }
+    
+    static func getProducts(by query: String, skip: Int) -> String {
+        var urlComponents = baseURLComponents
+        let paramsQueryItem = [
+            URLQueryItem(name: "q", value: query),
+            URLQueryItem(name: "limit", value: "\(ProductConstants.Network.limit)"),
+            URLQueryItem(name: "skip", value: "\(skip)")
+        ]
+        urlComponents.queryItems?.append(contentsOf: paramsQueryItem)
+        return urlComponents.url!
+            .appendingPathComponent(API.TypeOf.productsSearch)
+            .absoluteString
+    }
+    
+    static func addCart() -> String {
+        let urlComponents = baseURLComponents
+        return urlComponents.url!
+            .appendingPathComponent(API.TypeOf.addCart)
+            .absoluteString
+    }
+    
+    static func getUser() -> String {
+        let urlComponents = baseURLComponents
+        return urlComponents.url!
+            .appendingPathComponent(API.TypeOf.getUser)
             .absoluteString
     }
 }
