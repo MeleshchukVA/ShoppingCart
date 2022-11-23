966937d2104cb07a601892bd559b54c17bd30675
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-23 14:50:49 +0300

    Add UIApplication + Extension in UIKit folder in Helpers

---
+++ ShoppingCart/Sources/👷🏻‍♂️Helpers/😺UIKit/UIApplication + Extension.swift
@@ -0,0 +1,31 @@
+//
+//  UIApplication + Extension.swift
+//  ShoppingCart
+//
+//  Created by Владимир Мелещук on 22.11.2022.
+//
+
+import UIKit.UIApplication
+
+extension UIApplication {
+    var statusBarUIView: UIView? {
+        if #available(iOS 13.0, *) {
+            let tag = 38482458385
+            if let statusBar = self.keyWindow?.viewWithTag(tag) {
+                return statusBar
+            } else {
+                let statusBarView = UIView(frame: UIApplication.shared.statusBarFrame)
+                statusBarView.backgroundColor = Colors.purple
+                statusBarView.tag = tag
+
+                self.keyWindow?.addSubview(statusBarView)
+                return statusBarView
+            }
+        } else {
+            if responds(to: Selector(("statusBar"))) {
+                return value(forKey: "statusBar") as? UIView
+            }
+        }
+        return nil
+    }
+}
