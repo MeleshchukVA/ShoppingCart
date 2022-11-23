30d82e8539b473b7f6d2095f6b423dad67797559
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-23 14:55:17 +0300

    Add Assets in project. Remove SceneDelegate because of iOS 12. Edit AppDelegate

--- ShoppingCart/Sources/📱App/AppDelegate.swift
+++ ShoppingCart/Sources/📱App/AppDelegate.swift
@@ -8,4 +8,28 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate { }
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    private lazy var appDependency: AppDependency = AppDependency.makeDefault()
+    private var appCoordinator: AppCoordinator?
+    
+    func application(
+        _ application: UIApplication,
+        willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+        
+        appCoordinator = AppCoordinator(window: window, appDependency: appDependency)
+        appCoordinator?.start()
+        return true
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+}
