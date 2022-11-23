8da175167f0fb0af862aa04c0b40985aebd99bee
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-23 19:00:30 +0300

    Remove optional values in ProductCDModel+CoreDataProperties

--- ProductCDModel+CoreDataProperties.swift
+++ ProductCDModel+CoreDataProperties.swift
@@ -15,17 +15,17 @@ extension ProductCDModel {
         return NSFetchRequest<ProductCDModel>(entityName: PersistentConstants.productModel)
     }
 
-    @NSManaged public var brand: String?
-    @NSManaged public var category: String?
+    @NSManaged public var brand: String
+    @NSManaged public var category: String
     @NSManaged public var count: Int64
     @NSManaged public var discountPercentage: Double
     @NSManaged public var id: Int64
     @NSManaged public var images: [String]?
     @NSManaged public var isFavorite: Bool
     @NSManaged public var price: Int64
-    @NSManaged public var productDescription: String?
+    @NSManaged public var productDescription: String
     @NSManaged public var rating: Double
     @NSManaged public var stock: Int64
-    @NSManaged public var thumbnail: String?
-    @NSManaged public var title: String?
+    @NSManaged public var thumbnail: String
+    @NSManaged public var title: String
 }
