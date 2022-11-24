018e5a5aa41b62c4c2b135069d9f93c674e535a4
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-24 18:07:24 +0300

    Add comment in ProgrammaticallyInitializableViewProtocol

--- ShoppingCart/Sources/🧩Components/📜Protocols/ProgrammaticallyInitializableViewProtocol.swift
+++ ShoppingCart/Sources/🧩Components/📜Protocols/ProgrammaticallyInitializableViewProtocol.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// От данного протокола наследуются View и Cell в проекте.
 protocol ProgrammaticallyInitializableViewProtocol: AnyObject {
     func setupView()
     func setupSubviews()
