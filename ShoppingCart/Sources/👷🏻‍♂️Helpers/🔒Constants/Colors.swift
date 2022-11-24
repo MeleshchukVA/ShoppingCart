1caf83036fa98d621aff66d849b5599cacc00bdc
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-24 19:39:35 +0300

    Change colors order alphabetically in Colors

--- ShoppingCart/Sources/👷🏻‍♂️Helpers/🔒Constants/Colors.swift
+++ ShoppingCart/Sources/👷🏻‍♂️Helpers/🔒Constants/Colors.swift
@@ -9,17 +9,17 @@ import UIKit
 
 enum Colors {
     
-    static let lightWhite = UIColor.rgba(255, 255, 247)
-    static let lightGray = UIColor.rgba(28, 28, 30)
-    static let grayTabBar = UIColor.rgba(142, 142, 147)
     static let borderLightGray = UIColor.rgba(239, 239, 244)
+    static let cellColor = UIColor.rgba(36, 36, 38)
     static let grayDetail = UIColor.rgba(242, 242, 247)
+    static let grayTabBar = UIColor.rgba(142, 142, 147)
+    static let lightGray = UIColor.rgba(28, 28, 30)
+    static let lightWhite = UIColor.rgba(255, 255, 247)
     static let purple = UIColor.rgba(79, 13, 138)
     static let purple2 = UIColor.rgba(218, 143, 255)
     static let rose = UIColor.rgba(181, 18, 163)
-    static let cellColor = UIColor.rgba(36, 36, 38)
-    static let searchBarBackground = UIColor.rgba(62, 62, 66)
     static let rowFilterColor = UIColor.rgba(199, 199, 204)
+    static let searchBarBackground = UIColor.rgba(62, 62, 66)
 
     enum Gradient {
         
