3b34ff2c4ea27da7cb65ab13fba055a6977326a4
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-24 19:16:00 +0300

    Add markdown in TextField

--- ShoppingCart/Sources/🧩Components/TextField.swift
+++ ShoppingCart/Sources/🧩Components/TextField.swift
@@ -7,10 +7,14 @@
 
 import UIKit
 
+// MARK: - TextField Class
+
 final class TextField: UITextField {
     
-    let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-
+    // MARK: Properties
+    private let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    
+    // MARK: Override methods
     override func textRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: padding)
     }
