3757d59fc9f224ad582e0cae9f8b523b34dd0812
Vladimir Meleshchuk <vladimirmeleshuk@gmail.com>
2022-11-24 19:50:38 +0300

    Sort enums alphabetically in Localize

--- ShoppingCart/Sources/👷🏻‍♂️Helpers/🔒Constants/Localize.swift
+++ ShoppingCart/Sources/👷🏻‍♂️Helpers/🔒Constants/Localize.swift
@@ -9,20 +9,20 @@ import UIKit
 
 enum Localize {
     
-    static let catalogue = "Catalogue"
     static let cart = "Cart"
+    static let catalogue = "Catalogue"
     static let profile = "Profile"
 
     enum Images {
         
-        static let catalogueIcon: UIImage? = UIImage(named: "magnifyingglass")
         static let cartIcon: UIImage? = UIImage(named: "cart")
-        static let cartPlusIcon: UIImage? = UIImage(named: "cart.badge.plus")
         static let cartMinusIcon: UIImage? = UIImage(named: "cart.badge.minus")
-        static let profileIcon: UIImage? = UIImage(named: "person")
-        static let photo: UIImage? = UIImage(named: "photo")
-        static let minusIcon: UIImage? = UIImage(named: "minus.square.fill")
-        static let plusIcon: UIImage? = UIImage(named: "plus.square.fill")
+        static let cartPlusIcon: UIImage? = UIImage(named: "cart.badge.plus")
+        static let catalogueIcon: UIImage? = UIImage(named: "magnifyingglass")
         static let ellipsisIcon: UIImage? = UIImage(named: "ellipsis")
+        static let minusIcon: UIImage? = UIImage(named: "minus.square.fill")
+        static let photo: UIImage? = UIImage(named: "photo")
+        static let plusIcon: UIImage? = UIImage(named: "plus.square.fill")
+        static let profileIcon: UIImage? = UIImage(named: "person")
     }
 }
